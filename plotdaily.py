69cb66faf87898a290abfaf3059434b263e24e08
loqueelvientoajuarez <regis.lachaume@gmail.com>
2020-03-31 11:36:56 -0300

    fixes; +log & bw opts.

--- plotdaily.py
+++ plotdaily.py
@@ -4,19 +4,21 @@ import os
 import re
 import time
 import urllib.request
-import asciitable
+from astropy.io import ascii as asciitable
 import numpy as np
 import datetime
 from matplotlib import pylab as plt
 import argparse 
+from cycler import cycler
 
-def get_data(url, local, default_encoding='utf-8', max_time=2 * 3600):
+def get_data(url, local, default_encoding='utf-8-sig', max_time=2 * 3600):
+    # there should be a robust, built-in way to download stuff
     if (not os.path.exists(local) 
             or time.time() - os.path.getmtime(local) > max_time):
         print('Download data from', url)
         with urllib.request.urlopen(url) as response:
             encoding = response.headers.get_content_charset()
-            if encoding is None:
+            if encoding is None: # 
                 encoding = default_encoding 
             contents = response.read().decode(encoding)
         # we don't want to leave a half written file, so remove it
@@ -41,7 +43,7 @@ def get_country_data(tab, country, variable):
         if len(country) != 3:
             country_col = 'geoId'
     tab = tab[tab[country_col] == country]
-    # why is dealing with date such a pain?
+    # why is dealing with dates such a pain?
     tab_date = [datetime.datetime.strptime(d, '%d/%m/%Y').timestamp() / 86_400
                  for d in tab['dateRep']]
     tab_value = tab[variable + 's'].tolist()
@@ -61,20 +63,30 @@ def bin_data(date, value, date_origin=50, nbin=7):
     date = (date - date0)[nbin:]
     return date, binned
 
-def plot_countries(tab, countries, variable, date_origin=200, nbin=7):
-    ls = ['k-', 'b-', 'r-', 'm-', 'k--', 'b--', 'r--', 'm--']
+def plot_countries(tab, countries, variable, 
+        date_origin=200, nbin=7, logy=False, bw=False):
+    print('Plotting {} for {}'.format(variable + 's', ', '.join(countries)))
+    mycycler = cycler(linestyle=['-', '--', '-.', ':'])
+    if bw:
+        mycycler *= cycler(color=[(0, 0, 0), (.25, .25, .25), (.5, .5, .5)])
+    else:
+        mycycler *= cycler(color=['k', 'r', 'b', 'm']) 
     fig = plt.figure(1)
     fig.clf()
     fig.subplots_adjust(top=0.99,bottom=0.11, right=0.99)
     ax = fig.add_subplot(111)
-    ax.set_xlabel('Days since {}th {}'.format(date_origin, variable))
-    ax.set_ylabel('{}s in the last {} days'.format(variable, nbin))
+    ax.set_xlabel('days since {}th {}'.format(date_origin, variable))
+    ax.set_ylabel('new {}s in the last {} days'.format(variable, nbin))
+    ax.set_prop_cycle(mycycler)
+    if logy:
+        ax.set_yscale('symlog', linthreshy=1)
     for i, country in enumerate(countries):
         date, value = get_country_data(tab, country, variable)
         date, value = bin_data(date, value, date_origin=date_origin)
-        ax.plot(date, value, ls[i], label=country)
-        ax.text(1.01 * date[-1], 1.01 * value[-1], country, color=ls[i][0],
-            bbox=dict(facecolor='white', alpha=0.2, pad=1, lw=0))
+        p = ax.plot(date, value, label=country)
+        ax.text(1.01 * date[-1], 1.01 * value[-1], country, 
+            color=p[0].get_color(),
+            bbox=dict(facecolor='white', alpha=0.5, pad=1, lw=0))
     ax.set_xlim(-7, ax.get_xlim()[1])
     return fig
 
@@ -97,15 +109,23 @@ if __name__ == "__main__":
         default=50,
         help='given number of cases/deaths from which days are counted'
     )
+    parser.add_argument('-l', '--log', action='store_true', dest='logy', 
+        default=False,
+        help='semilog plot (by default: linear)'
+    )
     parser.add_argument('-f', '--format', dest='fmt', 
         default='pdf',  choices=['png', 'pdf'],
         help='plot format (pdf or png)',
+    )
+    parser.add_argument('--bw', action='store_true', 
+        default=False,  
+        help='black & white plot',
     )
     arg = parser.parse_args()
     url = 'https://opendata.ecdc.europa.eu/covid19/casedistribution/csv'
     csvname = 'covid-19.csv'
     pdfname = 'covid-19-{}s.{}'.format(arg.variable, arg.fmt)
-    tab = get_data(url, csvname, default_encoding='latin-1', max_time=7200)
+    tab = get_data(url, csvname, max_time=7200) # encoding changed overnight
     fig = plot_countries(tab, arg.countries, arg.variable, 
-            date_origin=arg.origin)
+            date_origin=arg.origin, logy=arg.logy, bw=arg.bw)
     fig.savefig(pdfname)
