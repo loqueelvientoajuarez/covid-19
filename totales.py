afc295eab9522443a64077b85a012ce3c50ba822
loqueelvientoajuarez <regis.lachaume@gmail.com>
2020-06-12 17:19:21 -0400

    casos y testeo chile

---
+++ totales.py
@@ -0,0 +1,102 @@
+#! /usr/bin/env python3
+
+import os
+import re
+import numpy as np
+from astropy.io import ascii
+from matplotlib import pylab as plt
+from matplotlib.dates import MO, WeekdayLocator, DateFormatter
+
+def read_time_series(product_number, header_lines=1, columns=None):
+    
+    subdir = 'producto{}'.format(product_number)
+    path = os.path.join('..', 'Datos-COVID19', 'output', subdir)
+    basenames = [f for f in os.listdir(path) if re.search('_T\.csv$', f)]
+    filename = os.path.join(path, basenames[0])
+
+    if header_lines == 1:
+        
+        tab = ascii.read(filename)
+        
+    else:    
+        
+        with open(filename, 'r') as input:
+            lines = input.read().splitlines()
+
+        names = lines[0].split(',')
+        for line in lines[1:header_lines]:
+            names = [n1 + ' ' + n2 for n1, n2 in zip(names, line.split(','))]
+
+        tab = ascii.read(lines[header_lines:], names=names)
+    
+    if columns is not None:
+        cols = tab.columns
+        return [cols[c].data for c in columns]
+    
+    return tab
+
+def week_average(x, y):
+    ndays = 7
+    l = len(y)
+    ymean = np.mean([y[ndays-1-i:l-i] for i in range(ndays)], axis=0)
+    x = x[ndays-1:]
+    return x, ymean
+
+def plot_date(axis, x, y, label, *a, **ka):
+    line = axis.plot(x, y, *a, **ka)[0]
+    color = line.get_color()
+    axis.set_ylabel(label, color=color)
+    axis.tick_params(axis='y', colors=color)
+    # ax.spines['right'].set_color(color)
+
+
+def plot_tests(style=None):
+
+    cols = [0, -2, -1]
+    test_d, test_t, test_n = read_time_series(17, header_lines=2, columns=cols)
+    cols = ['Fecha', 'Casos totales', 'Casos nuevos totales']
+    case_d, case_t, case_n = read_time_series(5, columns=cols)
+    case_d, case_n = week_average(case_d, case_n)
+    test_d, test_n = week_average(test_d, test_n)
+    pos_n = case_n[-len(test_n):] / test_n * 100
+    case_d = [np.datetime64(d) for d in case_d]
+    test_d = [np.datetime64(d) for d in test_d]
+
+    fig = plt.figure(1)
+    fig.clf()
+    if style:
+        plt.style.use(style)
+    
+    ax = fig.add_subplot(111)
+    plot_date(ax, case_d, case_n, 'casos nuevos diarios')
+    ax.set_ylim(0, max(case_n) * 1.02)
+    ax.set_xlim(min(case_d), max(case_d))
+   
+    ax2 = ax.twinx()
+    ax2.plot([], []) # advance the colour cycler
+    plot_date(ax2, test_d, pos_n, 'positividad de PCR [%]')
+    ax2.set_ylim(0, max(pos_n) * 1.1)
+    
+    ax3 = ax.twiny()
+    ax3.set_xlabel('Casos y testeo en Chile.  Promedio última semana.')
+    ax3.set_xticklabels([])
+ 
+    ax.tick_params(axis='x', rotation=30)
+    ax.xaxis.set_major_locator(WeekdayLocator(byweekday=MO, interval=2))
+    ax.xaxis.set_minor_locator(WeekdayLocator(byweekday=MO, interval=1))
+    ax.xaxis.set_major_formatter(DateFormatter('Lun. %d/%m'))
+    for label in ax.get_xticklabels():
+        label.set_ha('right')
+
+    if style == 'fivethirtyeight':
+        ax.grid(False)
+        ax2.grid(False)
+        ax3.grid(False)
+        ax.grid(axis='x', which='both')
+    #ax.grid(axis='y')
+    fig.tight_layout()
+    fig.show()
+    
+    fig.savefig('graphics/casos-chile.png')    
+
+plot_tests(style='fivethirtyeight')
